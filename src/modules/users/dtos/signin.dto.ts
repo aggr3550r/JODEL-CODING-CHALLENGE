b0eb50a32a0a22360abc55d3a07d789436185721
Victor Uche <ucheviquetor@gmail.com>
2022-04-09 00:08:01 +0100

    fixed merge conflicts

--- src/modules/users/dtos/signin.dto.ts
+++ src/modules/users/dtos/signin.dto.ts
@@ -1,10 +1,15 @@
-import { IsEmail, IsString } from "class-validator";
+import { IsEmail, IsNotEmpty, IsString } from "class-validator";
 
 export class SigninDTO {
+    @IsNotEmpty()
     @IsEmail()
     email: string;
+    
+    @IsNotEmpty()
     @IsString()
     username: string;
+
+    @IsNotEmpty()
     @IsString()
     password: string;
 }
