6a04a5f0807d53955fce4c6d798f3fcaa39d8c80
Victor Uche <ucheviquetor@gmail.com>
2022-07-05 11:57:13 +0100

    Fixed more tests

--- src/modules/results/__mocks__/result.service.ts
+++ src/modules/results/__mocks__/result.service.ts
@@ -1,7 +1,7 @@
-import { showResultStub } from "../test/stubs/show-result.stub";
-import { takeSurveyStub } from "../test/stubs/take-survey.stub";
+import { showResultStub } from '../test/stubs/show-result.stub';
+import { takeSurveyStub } from '../test/stubs/take-survey.stub';
 
 export const ResultService = jest.fn().mockReturnValue({
-    takeSurvey: jest.fn().mockReturnValue(takeSurveyStub()),
-    getResultsOfASurvey: jest.fn().mockReturnValue(showResultStub())
+  addAResultByTakingASurvey: jest.fn().mockReturnValue(takeSurveyStub()),
+  getResultsOfASurvey: jest.fn().mockReturnValue(showResultStub()),
 });
